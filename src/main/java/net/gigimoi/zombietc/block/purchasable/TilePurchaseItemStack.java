44043252efb853207ab8dfcb2f34b5fc27e49b42
gigimoi <gassmann.gavin@gmail.com>
2014-08-05 07:15:19 -0500

    Can no longer purchase item stacks in editor mode
    Signed-off-by: gigimoi <gassmann.gavin@gmail.com>

--- src/main/java/net/gigimoi/zombietc/block/purchasable/TilePurchaseItemStack.java
+++ src/main/java/net/gigimoi/zombietc/block/purchasable/TilePurchaseItemStack.java
@@ -1,6 +1,7 @@
 package net.gigimoi.zombietc.block.purchasable;
 
 import cpw.mods.fml.relauncher.Side;
+import net.gigimoi.zombietc.ZombieTC;
 import net.gigimoi.zombietc.block.ITileEntityActivatable;
 import net.gigimoi.zombietc.block.ITileEntityPurchasable;
 import net.gigimoi.zombietc.helpers.Lang;
@@ -72,7 +73,7 @@ public class TilePurchaseItemStack extends TileEntity implements ITileEntityActi
 
     @Override
     public boolean getEnabled() {
-        return true;
+        return !ZombieTC.editorModeManager.enabled;
     }
 
     @Override
