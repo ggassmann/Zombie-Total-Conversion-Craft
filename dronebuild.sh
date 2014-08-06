3ca04ad3630205573796700578c64c9ccdcb79c4
gigimoi <gassmann.gavin@gmail.com>
2014-08-06 08:30:51 -0500

    Added test drone build script
    Signed-off-by: gigimoi <gassmann.gavin@gmail.com>

---
+++ dronebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+chmod +x gradlew
+./gradlew build
+mkdir packaging
+cp build/libs/* packaging/
+cp -rf eclipse/mods/* packaging/
+mkdir out
+cd packaging
+mkdir mods
+mv *.jar mods 
+rm -rf "1.7.10"
+cd ../libs && cp *.jar ../packaging/mods && cd ../packaging
+tar czf ../out/zombie-total-conversion.tar.gz .
