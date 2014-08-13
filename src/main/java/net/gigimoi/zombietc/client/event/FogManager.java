20cfcaf42892521c2e4b0da8f4fa3f1db35098e0
gigimoi <gassmann.gavin@gmail.com>
2014-08-13 14:13:04 -0500

    Improved fog a bit
    Signed-off-by: gigimoi <gassmann.gavin@gmail.com>

--- src/main/java/net/gigimoi/zombietc/client/event/FogManager.java
+++ src/main/java/net/gigimoi/zombietc/client/event/FogManager.java
@@ -11,15 +11,15 @@ public class FogManager {
     @SubscribeEvent
     public void onFogDensity(EntityViewRenderEvent.FogDensity event) {
         event.setCanceled(true);
-        event.density = ZombieTC.editorModeManager.enabled ? 0.001f : 0.05f;
+        event.density = ZombieTC.editorModeManager.enabled ? 0.0001f : 0.03f;
     }
 
     @SubscribeEvent
     public void onFogColor(EntityViewRenderEvent.FogColors event) {
         if (!ZombieTC.editorModeManager.enabled) {
-            event.red = 0f;
-            event.blue = 0f;
-            event.green = 0f;
+            event.red = 0.25f;
+            event.blue = 0.25f;
+            event.green = 0.25f;
         }
     }
 }
