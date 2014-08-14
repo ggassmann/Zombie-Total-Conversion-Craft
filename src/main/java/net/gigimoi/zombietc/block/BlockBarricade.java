5232ef2830f4f8b13479ff7d44283b7ef5fdf75e
gigimoi <gassmann.gavin@gmail.com>
2014-08-14 17:23:53 -0500

    NOTHING TO SEE HERE MOVE ALONG
    Signed-off-by: gigimoi <gassmann.gavin@gmail.com>

--- src/main/java/net/gigimoi/zombietc/block/BlockBarricade.java
+++ src/main/java/net/gigimoi/zombietc/block/BlockBarricade.java
@@ -30,7 +30,7 @@ public class BlockBarricade extends BlockContainerZTC {
     }
 
     @Override
-    public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
+    public TileEntity createNewTileEntity(World world, int meta) {
         return new TileBarricade();
     }
 
